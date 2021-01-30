a399747f34e1e3c1a8d46502fc52cc272e4e4d42
etomaki <70105692+etomaki@users.noreply.github.com>
2021-01-30 20:11:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from n import Cat, Circle
+
+cat_1 = Cat('Барон', 'Мальчик', 2)
+cat_2 = Cat('Сэм', 'Мальчик', 2)
+
+# print(cat_2.name)
+# print(cat_2.getName())
+
+circ_1 = Circle(10)
+circ_2 = Circle(14)
+
+print(circ_1.get_circle_area())
+print(circ_2.get_circle_area())
